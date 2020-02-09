180d158b4d3090b8a2687dddcfcafb3be3f7c2bf
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-09 21:56:16 +0100

    Add files via upload

---
+++ 45. STRREV/45. Strrev.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	char cad1[]="reconocer";
+	
+	strrev(cad1);
+	
+	cout<<cad1<<endl;
+	
+	cout<<"\n";
+	getch();
+	return 0;
+}
