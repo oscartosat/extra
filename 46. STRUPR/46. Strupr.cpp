af096e69443be875b366d216d30f74a44ca7c8d0
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-09 22:25:40 +0100

    Add files via upload

---
+++ 46. STRUPR/46. Strupr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	char palabra[]="oscar";
+	
+	strupr(palabra);
+	
+	cout<<palabra<<endl;
+	
+	getch();
+	return 0;
+}
