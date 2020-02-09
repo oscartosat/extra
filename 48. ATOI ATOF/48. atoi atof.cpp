5c70389b3a9f475129dd68b971ee57664283fa9a
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-09 22:49:13 +0100

    Add files via upload

---
+++ 48. ATOI ATOF/48. atoi atof.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+	char cad[]="123";
+	int numero;
+	
+	numero=atoi(cad);
+	
+	cout<<numero<<endl;
+	
+	getch();
+	return 0;
+}
