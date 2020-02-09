0400efe98563cf95fbe00af10634f1213cf97695
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-09 21:47:11 +0100

    Add files via upload

---
+++ CAMBIAR COLORES/Cambiar Colores.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+	system("color 5A");
+	
+	cout<<"\n\tHola mundo";
+	
+	getch();
+	return 0;
+}
+
+/*
+#include<stdlib.h>
+
+system("color 0B");
+
+->Color FOMDO
+	0 = Negro
+	1 = Azul
+	2 = Verde
+	3 = Aguamarina
+	4 = Rojo
+	5 = Purpura
+	6 = Amarillo
+	7 = Blanco
+	8 = Gris
+	9 = Azul Claro
+	
+->Color LETRA
+	A = Verde Claro
+	B = Aguamarina Claro
+	C = Rojo Claro
+	D = Purpura Claro
+	E = Amarillo Claro
+	F = Blanco Brillante
+	
+*/
+	
