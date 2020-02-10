0ffc56947c8f87361488c2f3a44d0430227a3b3c
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-10 20:46:00 +0100

    Add files via upload

---
+++ 51. ORDENAMIENTO POR INSERCION/51. Ordenamiento por insercion.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int numeros[]={4,2,3,1,5};
+	int i,pos,aux;
+	
+	for(i=0;i<5;i++){
+		pos=i;
+		aux=numeros[i];
+		
+		while((pos>0)&&(numeros[pos-1]>aux)){
+			numeros[pos]=numeros[pos-1];
+			pos--;
+		}
+		numeros[pos]=aux;
+	}
+	
+	cout<<"Orden ascendente: ";
+	for(i=0;i<5;i++){
+		cout<<numeros[i]<<" ";
+	}
+	
+	cout<<"\nOrden ascendente: ";
+	for(i=4;i>=0;i--){
+		cout<<numeros[i]<<" ";
+	}
+	
+	getch();
+	return 0;
+}
