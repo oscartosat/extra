73682fdcb6e3ecb2b8be3de64317c218f005734d
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-10 22:08:43 +0100

    Add files via upload

---
+++ 54. BUSQUEDA BINARIA/54.Busqueda binaria.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int numeros[]={1,2,3,4,5};
+	int inf,sup,mitad,dato;
+	char band='F';
+	
+	dato=2;
+	
+	//Algoritmo de la busqueda binaria
+	inf=0;
+	sup=5;
+	
+	while(inf<=sup){
+		mitad=(inf+sup)/2;
+		
+		if(numeros[mitad]==dato){
+			band='V';
+			break;
+		}
+		if(numeros[mitad]>dato){
+			sup=mitad;
+			mitad=(inf+sup)/2;
+		}
+		if(numeros[mitad]<dato){
+			inf=mitad;
+			mitad=(inf+sup)/2;
+		}
+	}
+	
+	if(band=='V'){
+		cout<<"El numero ha sido encontrado en la posicion: "<<mitad<<endl;
+	}
+	else{
+		cout<<"El numero no ha sido encontrado"<<endl;
+	}
+	
+	getch();
+	return 0;
+}
