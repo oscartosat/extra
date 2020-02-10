6a853c1a5365faa1656267e955eb265d765858c8
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-10 20:23:31 +0100

    Add files via upload

---
+++ 50. METODO BURBUJA/50. Metodo burbuja.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int numeros[]={4,1,2,3,5};
+	int i, j, aux;
+	
+	//Algortimo metodo burbuja
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if(numeros[j]>numeros[j+1]){
+				aux=numeros[j];
+				numeros[j]=numeros[j+1];
+				numeros[j+1]=aux;
+			}
+		}
+	}
+	
+	cout<<"Orden ascendente: ";
+	for(i=0;i<5;i++){
+		cout<<numeros[i]<<" ";
+	}
+	
+	cout<<"\nOrden descendente: ";
+	for(i=4;i>=0;i--){
+		cout<<numeros[i]<<" ";
+	}
+	
+	getch();
+	return 0;
+}
