c7992e5f664d566b9d6e704a919fe1425ee7e604
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-11 22:45:10 +0100

    Add files via upload

---
+++ 69. DEVOLVER VALORES MULTIPLES/69. Funcion devolver valores multiples.cpp
@@ -0,0 +1,30 @@
+//Devolver valores multiples
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void calcular(int,int,int&,int&);
+
+
+int main(){
+	int num1,num2,suma=0,producto=0;
+	
+	cout<<"Introduzca 2 numeros: ";
+	cin>>num1>>num2;
+	
+	calcular(num1, num2, suma, producto);
+	
+	cout<<"El valor de la suma es: "<<suma<<endl;
+	cout<<"El valor de la multiplicacion es: "<<producto<<endl;
+	
+	getch();
+	return 0;
+}
+
+void calcular(int num1,int num2,int& suma,int& producto){
+	suma=num1+num2;
+	producto=num1*num2;
+	
+}
