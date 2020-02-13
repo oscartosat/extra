bc2af47f69eba51ab19bfddd246ed884a97290c8
Oscar Tosat <55576392+oscartosat@users.noreply.github.com>
2020-02-13 18:42:38 +0100

    Add files via upload

---
+++ 84. PUNTERO Y VECTOR/84. Puntero y vector.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int numeros[]={1,2,3,4,5};
+	int *dir_numeros;
+	
+	dir_numeros=numeros;
+	
+	for(int i=0;i<5;i++){
+		cout<<"Elemento del vector ["<<i<<"]: "<<*dir_numeros++<<endl;
+		cout<<"Posicion de memoria: "<<dir_numeros++<<endl;
+	}
+	
+	getch();
+	return 0;
+}
